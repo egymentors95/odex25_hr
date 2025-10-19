264f0003b197a08612eb929b80791f416b6a6bc3
mohamed <ibs.developer1@gmail.com>
2025-10-20 00:29:35 +0300

    asd

--- hr_base/models/res_partner_bank.py
+++ hr_base/models/res_partner_bank.py
@@ -4,5 +4,8 @@ from odoo import models, fields
 class ResPartnerBank(models.Model):
     _inherit = 'res.partner.bank'
 
-    employee_id = fields.Many2one(comodel_name='hr.employee', string="Employee")
+    employee_id = fields.Many2one(comodel_name='hr.employee', string="اسم مالك الحساب")
     default_number = fields.Boolean(string="الافتراضي")
+    partner_id = fields.Many2one('res.partner', 'Account Holder', ondelete='cascade', index=True, domain=['|', ('is_company', '=', True), ('parent_id', '=', False)], required=False)
+
+
